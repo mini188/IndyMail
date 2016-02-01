1790f4e39c6fddaf61c8dcfcac0892d77d04f116
mini188 <mini188.com@qq.com>
2016-02-01 17:25:26 +0800

    Update IdSMTPEx.pas

--- IdSMTPEx.pas
+++ IdSMTPEx.pas
@@ -3,7 +3,7 @@
 { for Delphi base Indy9                                                     }
 {                                                                           }
 { 继承TIdSMTP组件用于解决以下问题：                                         }
-{   1、SendBody方法同时发送TIdText和TIdAttachment时TIdText发送失败问题      }
+{   1、SendBody方法同时发送TIdText和TIdAttachment时TIdText不发送的问题      }
 {   2、SendHeader主题长度被截断问题                                         }
 { written by mini188                                                        }
 { Email : mini188.com@qq.com                                                }
